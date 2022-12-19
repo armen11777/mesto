4dfbdf8b3402c022d77fa6d9629627a9b549a2c4
Армен Погосян <armen-pogosyan@mail.ru>
2022-12-19 21:28:43 +0300

    2-я проверка

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,14 @@ let formElement = document.querySelector('.popup__form-edit');
 let nameInput = formElement.querySelector('#name_text');
 let jobInput = formElement.querySelector('#description_text');
 
+function handleFormSubmit (evt) {
+  evt.preventDefault(); 
+  document.querySelector('.profile__title').textContent = nameInput.value;
+  document.querySelector('.profile__text').textContent = jobInput.value;
+  popup.classList.remove('popup_window-opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
 
 editButton.addEventListener('click', () => {
   popup.classList.add('popup_window-opened');
@@ -16,24 +24,3 @@ editButton.addEventListener('click', () => {
 closePopup.addEventListener('click', ()=> {
   popup.classList.remove('popup_window-opened');
 })
-
-// Обработка нажатия кнопки "Лайк"
-likeButton.addEventListener('click', (event) => {
-  let target = event.target;
-  if (target.tagName != 'BUTTON') return;
-  if (target.classList[1] ==='element__like-button_status_active') {
-    target.classList.remove('element__like-button_status_active');
-  }
-  else 
-  {target.classList.add('element__like-button_status_active');
-}
-})
-
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-    document.querySelector('.profile__title').textContent = nameInput.value;
-    document.querySelector('.profile__text').textContent = jobInput.value;
-    popup.classList.remove('popup_window-opened');
-}
-
-formElement.addEventListener('submit', handleFormSubmit); 

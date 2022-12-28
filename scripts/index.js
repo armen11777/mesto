d91a643b5ad2001ecfc79ea5342f9aacc4dd960b
Армен Погосян <armen-pogosyan@mail.ru>
2022-12-29 01:59:45 +0300

    Добавление карточек и лайков

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ let likeButton = document.querySelector('.elements');
 let formElement = document.querySelector('.popup__form-edit');
 let nameInput = formElement.querySelector('#name_text');
 let jobInput = formElement.querySelector('#description_text');
+const elementsList = document.querySelector('.elements-list');
 
 function handleFormSubmit (evt) {
   evt.preventDefault(); 
@@ -24,3 +25,44 @@ editButton.addEventListener('click', () => {
 closePopup.addEventListener('click', ()=> {
   popup.classList.remove('popup_window-opened');
 })
+
+likeButton.addEventListener('click', (evt)=> {
+  evt.target.classList.toggle('element__like-button_status_active');
+})
+
+const elementTemplate = document.querySelector('#element-template').content;
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+for (let i =0; i < initialCards.length; i+=1) {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = initialCards[i].link;
+  element.querySelector('.element__title').textContent = initialCards[i].name;
+  elementsList.append(element);
+}
